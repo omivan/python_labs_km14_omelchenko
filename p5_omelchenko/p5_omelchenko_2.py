ca367cba7ae3186f6a0d9737d5c413baadf374c0
Ivan Omelchenko <ivan.omelchenk@gmail.com>
2021-10-04 19:51:54 +0300

    p5_2 added

---
+++ p5_omelchenko/p5_omelchenko_2.py
@@ -0,0 +1,11 @@
+try:
+    word_list = []
+     
+    while True:
+        word_list.append((input()))
+        
+except:
+    for i in range(len(word_list)):
+        if(i == len(word_list) - 2): print(word_list[i], end = ' and ') 
+        elif(i == len(word_list) - 1): print(word_list[i])
+        else: print(word_list[i], end = ', ') 
