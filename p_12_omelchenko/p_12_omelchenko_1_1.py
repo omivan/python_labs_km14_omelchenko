54a52d1aad74746fb8238383358ab09bb9126d17
Ivan Omelchenko <ivan.omelchenk@gmail.com>
2021-11-26 22:30:04 +0200

    p_12 edited

--- p_12_omelchenko/p_12_omelchenko_1_1.py
+++ p_12_omelchenko/p_12_omelchenko_1_1.py
@@ -1,4 +1,7 @@
-from itertools import chain
+
+import itertools
+
+import functools
 dirs = [
     ( 'folder1',
         [
@@ -22,13 +25,23 @@ dirs = [
 ]
 
 # ВАШ КОД ТУТ
-number_of_nodes = list(chain(["a", "b", ["c", "d", ["e", ["g"]]]]))
-print(number_of_nodes)
-def search(dirs, filename):
-    pass
-    
+
+def search (dirs, filename, path = ""):
+    res = []
+    if type(dirs) == tuple:
+        if len(path)==0:
+            path += "/" + dirs[0]
+        else:
+            path += "//" + dirs[0]
+        dirs = dirs[1]
+    if type(dirs) == tuple or type(dirs) == list:
+        res.extend(functools.reduce(lambda res, dir: res + search(dir, filename, path), dirs, []))
+    if dirs == filename:
+        res.append("{}/{}".format(path, filename))
+    return res
+#search(dirs, "file3")
 # ПЕРЕВІРКА
-"""
+
 print(search(dirs, 'file1'))
 print(search(dirs, 'file2'))
 print(search(dirs, 'file3'))
@@ -45,4 +58,3 @@ assert search(dirs, 'file5') == ['/folder1/file5'], 'Failed test for file5'
 assert search(dirs, 'file6') == [], 'Failed test for file6'
 assert search(dirs, 'folder1') == [], 'Failed test for folder1'
 print('All tests good!')
-"""
