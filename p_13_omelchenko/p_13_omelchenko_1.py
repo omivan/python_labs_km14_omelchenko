fd1616f168a9bc794f164856f59fa7866d62dc0a
Ivan Omelchenko <ivan.omelchenk@gmail.com>
2021-11-29 16:10:01 +0200

    p_13_1 added

--- p_13_omelchenko/p_13_omelchenko_1.py
+++ p_13_omelchenko/p_13_omelchenko_1.py
@@ -1,1 +1,19 @@
+from collections import defaultdict
+char_counter = defaultdict(int)
 file = open("gadsby.txt", 'r')
+text = file.readlines()
+number_of_all_letters = 0
+for line in text:
+    line = line.lower()
+    for char in line:
+        if char.isalpha():
+            char_counter[char] += 1
+            number_of_all_letters += 1
+sorted_char_counter = sorted(char_counter.items(), key = lambda x: x[1])
+sorted_char_counter.reverse()
+print(sorted_char_counter)
+for i in range(0, 5):
+    print(str(sorted_char_counter[i][0]) + " - " + str(round(sorted_char_counter[i][1]/number_of_all_letters*100, 3)) + " %")
+print(".\n.\n.\n.")
+for i in range(len(sorted_char_counter) - 5, len(sorted_char_counter)):
+    print(str(sorted_char_counter[i][0]) + " - " + str(round(sorted_char_counter[i][1]/number_of_all_letters*100, 3)) + " %")
