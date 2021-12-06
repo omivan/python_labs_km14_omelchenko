f1b38d29f692a699036a5330f7aa54ee5dbbd339
omivan <ivan.omelchenk@gmail.com>
2021-12-06 19:25:03 +0200

    p_14_2 added

---
+++ p_14_omelchenko/p_14_omelchenko_2.py
@@ -0,0 +1,22 @@
+import json
+with open('p_14_omelchenko\\image_info_test-dev2017.json') as f:
+    data = json.load(f)
+
+print("1) " + str(len(data["images"])))
+print("2) " + str(len(data["categories"])))
+print('3)', end='')
+first = list()
+for x in data['images']:
+    if x['file_name'] == "000000000001.jpg":
+        print("coco_url = " + str(x["coco_url"]) + " | height = " + str(x["height"]) + " | width = " + str(x["width"]) + " | id = " + str(x['id']))
+max_id = 0
+name_max_id = ""
+for x in data["images"]:
+    if(int(x['id']) > max_id): 
+        max_id = x['id'] 
+        name_max_id = x['file_name']
+print('4)Max id has ' + name_max_id)
+
+
+
+
