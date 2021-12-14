dd0b643870413e3b345d28be326ec438ce2f8b62
omivan <ivan.omelchenk@gmail.com>
2021-12-14 11:11:01 +0200

    p_15_2 edited

--- p15_omelchenko/p15_omelchenko_2.py
+++ p15_omelchenko/p15_omelchenko_2.py
@@ -1,9 +1,9 @@
 card_number = ('A', *range(2, 10), 'J', 'Q', 'K')
 card_suit = ('diamonds', 'clubs', 'hearts', 'spades')
 def card_shuffle():
-    for x in card_number:
-        for y in card_suit:
-            yield(str(x) + " " + y)
+    for x in card_suit:
+        for y in card_number:
+            yield(str(y) + " " + x)
 card_generator = card_shuffle()
 while(True):
     print(next(card_generator))
